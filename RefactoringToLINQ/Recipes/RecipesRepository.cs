e1dd571f605d61a2e10d9115a95734a05f910e3b
Talm28 <100356396+Talm28@users.noreply.github.com>
2024-09-28 16:10:13 +0300

    Add RefactoringToLINQ assignment.

---
+++ RefactoringToLINQ/Recipes/RecipesRepository.cs
@@ -0,0 +1,41 @@
+﻿using CookieCookbook.DataAccess;
+using CookieCookbook.Recipes.Ingredients;
+
+namespace CookieCookbook.Recipes;
+
+public class RecipesRepository : IRecipesRepository
+{
+    private readonly IStringsRepository _stringsRepository;
+    private readonly IIngredientsRegister _ingredientsRegister;
+    private const string Separator = ",";
+
+    public RecipesRepository(
+        IStringsRepository stringsRepository,
+        IIngredientsRegister ingredientsRegister)
+    {
+        _stringsRepository = stringsRepository;
+        _ingredientsRegister = ingredientsRegister;
+    }
+
+    public List<Recipe> Read(string filePath)
+    {
+        List<string> recipesFromFile = _stringsRepository.Read(filePath);
+        return recipesFromFile
+            .Select(recipeFromFile => RecipeFromString(recipeFromFile))
+            .ToList();
+    }
+
+    private Recipe RecipeFromString(string recipeFromFile)
+    {
+        var ingredient = recipeFromFile.Split(Separator)
+            .Select(textualId => _ingredientsRegister.GetById(int.Parse(textualId)));
+        return new Recipe(ingredient);
+    }
+
+    public void Write(string filePath, List<Recipe> allRecipes)
+    {
+        _stringsRepository.Write(filePath, allRecipes
+            .Select(recipe => string.Join(Separator, recipe.Ingredients
+                .Select(ingredient => ingredient.Id))).ToList());
+    }
+}
