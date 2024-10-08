3a7d1a9205ecc6c7eff4720892fa349550af9377
Talm28 <100356396+Talm28@users.noreply.github.com>
2024-10-08 17:05:46 +0300

    Add NetUnderTheHoodAssignment assignment.

---
+++ NetUnderTheHoodAssignment/NewSolution/FastRow.cs
@@ -0,0 +1,44 @@
+﻿namespace CsvDataAccess.NewSolution;
+
+public class FastRow
+{
+    private Dictionary<string, bool> _dataBool;
+    private Dictionary<string, int> _dataInt;
+    private Dictionary<string, decimal> _dataDecimal;
+    private Dictionary<string, string> _dataString;
+
+    public FastRow()
+    {
+        _dataBool = new Dictionary<string,bool>();
+        _dataInt = new Dictionary<string,int>();
+        _dataDecimal = new Dictionary<string,decimal>();
+        _dataString = new Dictionary<string,string>();
+    }
+
+    public void AssignCell(string columnName, bool value)
+    {
+        _dataBool[columnName] = value;
+    }
+    public void AssignCell(string columnName, int value)
+    {
+        _dataInt[columnName] = value;
+    }
+    public void AssignCell(string columnName, decimal value)
+    {
+        _dataDecimal[columnName] = value;
+    }
+    public void AssignCell(string columnName, string value)
+    {
+        _dataString[columnName] = value;
+    }
+
+
+    public object GetAtColumn(string columnName)
+    {
+        if(_dataBool.ContainsKey(columnName)) return _dataBool[columnName];
+        else if(_dataInt.ContainsKey(columnName)) return _dataInt[columnName];
+        else if(_dataDecimal.ContainsKey(columnName)) return _dataDecimal[columnName];
+        else if(_dataString.ContainsKey(columnName)) return _dataString[columnName];
+        return null;
+    }
+}
---
+++ NetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
@@ -0,0 +1,73 @@
+﻿using CsvDataAccess.CsvReading;
+using CsvDataAccess.Interface;
+
+namespace CsvDataAccess.NewSolution;
+
+public class FastTableDataBuilder : ITableDataBuilder
+{
+     public ITableData Build(CsvData csvData)
+    {
+        var resultRows = new List<FastRow>();
+
+        foreach (var row in csvData.Rows)
+        {
+            FastRow fastRow = new FastRow();
+            for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
+            {
+                var column = csvData.Columns[columnIndex];
+                string valueAsString = row[columnIndex]; 
+                
+                // Convert and insert the value to the row
+                if(string.IsNullOrEmpty(valueAsString))
+                    continue;
+                else if (valueAsString == "TRUE") 
+                {
+                    fastRow.AssignCell(column, true);
+                }
+                else if (valueAsString == "FALSE")
+                {
+                    fastRow.AssignCell(column, false);
+                }
+                else if (int.TryParse(valueAsString, out var valueAsInt))
+                {
+                    fastRow.AssignCell(column, valueAsInt);
+                }
+                else if (valueAsString.Contains(".") && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                {
+                    fastRow.AssignCell(column, valueAsDecimal);
+                }
+                else
+                    fastRow.AssignCell(column, valueAsString);
+            }
+
+            resultRows.Add(fastRow);
+        }
+
+        return new FastTableData(csvData.Columns, resultRows);
+    }
+
+
+    private void InsertValueToFastRow(string value, FastRow fastRow, string column)
+    {
+        if(string.IsNullOrEmpty(value))
+            return;
+        else if (value == "TRUE") 
+        {
+            fastRow.AssignCell(column, true);
+        }
+        else if (value == "FALSE")
+        {
+            fastRow.AssignCell(column, false);
+        }
+        else if (int.TryParse(value, out var valueAsInt))
+        {
+            fastRow.AssignCell(column, valueAsInt);
+        }
+        else if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
+        {
+            fastRow.AssignCell(column, valueAsDecimal);
+        }
+        else
+            fastRow.AssignCell(column, value);
+    }
+}
